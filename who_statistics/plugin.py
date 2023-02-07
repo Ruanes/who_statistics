1d16ae309da82ad77cb3524f180b359c2f01a78c
Runar E <runar_espelund@outlook.com>
2023-02-07 15:40:09 +0100

    Added comments

--- who_statistics/plugin.py
+++ who_statistics/plugin.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 class WhoStatistics(object):
 
+    # Simple command that just returns pong
     @command(description='Returns pong')
     def ping(self):
         return 'pong'
@@ -11,41 +12,54 @@ class WhoStatistics(object):
     # Returns statistics for <country> in <year>
     @command(description='Returns statistics for <country> in <year>')
     def get_stat(self, country, year):
+        # Read the .csv file 'LifeExpectancyData.csv and store as a dataframe object
         rf = pd.read_csv('LifeExpectancyData.csv')
+        # Find location of <country> and <year> in the dataframe
         stat = rf.loc[(rf['Country'] == country) & (rf['Year'] == year)]
+        # Returns as a normal python list
         return stat.round().values.tolist()
 
     # Returns the latest statistics for <country>
     @command(description='Returns the latest statistics for <country>')
     def get_latest(self, country):
         rf = pd.read_csv('LifeExpectancyData.csv')
+        # Store all data from <country> in the dataframe object
         stat = rf.loc[rf['Country'] == country]
+        # Return latest and convert to python list (rounded values for easy of use)
         return stat.iloc[[0]].round().values.tolist()
 
     # Returns the average statistics for <country>
     @command(description='Returns the average statistics for <country>')
     def get_avg(self, country):
+        # Read the csv file and store as a dataframe object
         rf = pd.read_csv('LifeExpectancyData.csv')
+        # Store all data from <country>
         stat = rf.loc[rf['Country'] == country]
+        # Extract all values from the dataframe and get average values using .mean()
         average_stat = stat[['Life expectancy', 'Adult Mortality', 'Infant deaths', 'Alcohol',
                              "Percentage expenditure", "Hepatitis B", "Measles", "BMI",
                              "Under-five deaths", "Polio", "Total expenditure", "Diphtheria",
                              "HIV/AIDS", "GDP", "Population", "Thinness 1-19 years",
                              "Thinness 5-9 years", "Income composition of resources", "Schooling"]].mean()
+        # Return rounded values as a normal python list
         return average_stat.round().values.tolist()
 
     # Returns the average statistics for <region> in <year>
     @command(description='Returns the average statistics for <region> in <year>')
     def get_avg_region(self, region, year):
         rf = pd.read_csv('LifeExpectancyData.csv')
+        # Read the 'countriesoftheworld.csv' and store as a dataframe
         rf_regions = pd.read_csv('countriesoftheworld.csv')
+        # Adding the two dataframe objects to one
         df_merged = pd.merge(rf, rf_regions)
+        # Extract all selected rows  in <region> and <year> and find average values
         stat = df_merged.loc[(df_merged['Region'] == region) & (df_merged['Year'] == year)]
         average_stat_region = stat[['Life expectancy', 'Adult Mortality', 'Infant deaths', 'Alcohol',
                                     "Percentage expenditure", "Hepatitis B", "Measles", "BMI",
                                     "Under-five deaths", "Polio", "Total expenditure", "Diphtheria",
                                     "HIV/AIDS", "GDP", "Population", "Thinness 1-19 years",
                                     "Thinness 5-9 years", "Income composition of resources", "Schooling"]].mean()
+        # Return rounded numbers as a list
         return average_stat_region.round().values.tolist()
 
 
